04abec49eb466321eaea7b7f8a41f1f2a4fff97d
DevPolarhub <68076124+DevPolarhub@users.noreply.github.com>
2022-11-10 11:00:03 +0700

    :tada:: init

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+// vite.config.js
+import { sveltekit } from '@sveltejs/kit/vite'
+
+/** @type {import('vite').UserConfig} */
+const config = {
+    server: {
+        port: 3000
+    },
+    plugins: [sveltekit()]
+}
+
+export default config
